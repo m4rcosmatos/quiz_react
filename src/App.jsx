c124af52ad894debbd8a7efa6d5e77560529c6d9
m4rcosmatos <marcosmattos026@gmail.com>
2024-08-19 15:46:20 -0300

    Implementando a escolha de categoria

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,16 @@ import { QuizContext } from "./context/quiz";
 import Welcome from "./components/Welcome";
 import Question from "./components/Question";
 import GameOver from "./components/GameOver";
+import PickCategory from "./components/PickCategory";
 
 function App() {
   const [quizState, dispatch] = useContext(QuizContext);
-
-  useEffect(() => {
-    dispatch({ type: "REORDER_QUESTIONS" });
-  }, []);
 
   return (
     <div className="App">
       <h1>Quiz App</h1>
       {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
       {quizState.gameStage === "Playing" && <Question />}
       {quizState.gameStage === "End" && <GameOver />}
     </div>
